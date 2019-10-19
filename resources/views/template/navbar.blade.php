58952ea575cbc7c9b77ffea8822c0bd6ed4ce789
root <root@kuronekosan.web.id>
2019-10-19 14:15:10 +0800

    Change Template

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-    <a class="navbar-brand mr-1" href="index.html">BEM FT UTS</a>
+    <a class="navbar-brand mr-1" href="{{ url('/dashboard') }}">BEM FT UTS</a>
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
     </button>
